1e43d23eb1d43e82b084b8c6263cc5e5db19bed2
Charlie Beckwith <charlie.beckwith@marquette.edu>
2014-02-27 12:22:44 -0600

    Added debug options for player. May be usefull later. Kind of in line with what you did earlier with making offsetX and offsetY. Which I had pulled that in earlier lol.
    Check it out though.

--- Hydrogen Hank/src/com/hh/objects/Player.java
+++ Hydrogen Hank/src/com/hh/objects/Player.java
@@ -2,6 +2,7 @@ package com.hh.objects;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
 import java.util.Random;
 
 import com.hh.Game;
@@ -16,119 +17,138 @@ import com.hh.input.KeyInput;
 /**
  * COSC3550 Spring 2014 Homework 3
  * 
- * Created : Feb. 7, 2014
- * Last Updated : Feb. 11, 2014
- * Purpose: Defines the Dust Bunny
+ * Created : Feb. 7, 2014 Last Updated : Feb. 11, 2014 Purpose: Defines the Dust
+ * Bunny
  * 
  * @author Mark Schlottke
  */
 @SuppressWarnings("unused")
-public class Player extends GameObject
-{
-  private final float GRAVITY = 200f;
+public class Player extends GameObject {
+	private final float GRAVITY = 200f;
 
-  private Animation RIGHT, CURRENT;
-  private ArtAssets art;
-  private float BUOYANCY;
-  private Color HUE;
+	private Animation RIGHT, CURRENT;
+	private ArtAssets art;
+	private float BUOYANCY;
+	private Color HUE;
+	private float startx, starty;
+	private LinkedList<String> debugOptions;
 
-  public Player(float x, float y, int width, int height, Vector2D v)
-  {
-    super(x, y, width-30, height, v, ObjectID.Player, ObjectLayer.foreground);
+	public Player(float x, float y, int width, int height, Vector2D v) {
+		super(x, y, width - 30, height, v, ObjectID.Player,
+				ObjectLayer.foreground);
+		startx = x;
+		starty = y;
+		ALIVE = true;
+		Random rand = new Random();
+		HUE = new Color(50 + rand.nextInt(200), 50 + rand.nextInt(200),
+				50 + rand.nextInt(200));
+		BUOYANCY = 0.0f;
+		art = Game.getArtAssets();
+		initAnimations();
+	}
 
-    ALIVE = true;
-    Random rand = new Random();
-    HUE = new Color(50 + rand.nextInt(200), 50 + rand.nextInt(200), 50 + rand.nextInt(200));
-    
-    BUOYANCY = 0.0f;
-    art = Game.getArtAssets();
-    initAnimations();
-  }
-  
+	public void tick() {
+		if (ALIVE) {
+			boolean collision = collision();
 
-  public void tick()
-  {
-    if (ALIVE)
-    {
-      boolean collision = collision();
+			if (KeyInput.KEYSDOWN.contains(KeyBinding.INFLATE.VALUE())) {
+				BUOYANCY -= 5.1f;
+			}
 
-      if (KeyInput.KEYSDOWN.contains(KeyBinding.INFLATE.VALUE()))
-      {
-        BUOYANCY -= 5.1f;
-      }
+			if (KeyInput.KEYSDOWN.contains(KeyBinding.DEFLATE.VALUE())) {
+				BUOYANCY += 1.1f;
+			}
 
-      if (KeyInput.KEYSDOWN.contains(KeyBinding.DEFLATE.VALUE()))
-      {
-        BUOYANCY += 1.1f;
-      }
+			if (BUOYANCY > 0) {
+				BUOYANCY = 0;
+			}
+			if (BUOYANCY < -(GRAVITY * 2)) {
+				BUOYANCY = -(GRAVITY * 2);
+			}
 
-      if (BUOYANCY > 0)
-      {
-        BUOYANCY = 0;
-      }
-      if (BUOYANCY < -(GRAVITY * 2))
-      {
-        BUOYANCY = -(GRAVITY * 2);
-      }
+			if (V.DX < 5000 && !collision
+					&& KeyInput.KEYSDOWN.contains(KeyBinding.FANON.VALUE())) {
+				V.DX += 1;
+			}
 
-      if (V.DX < 5000 && !collision && KeyInput.KEYSDOWN.contains(KeyBinding.FANON.VALUE()))
-      {
-        V.DX += 1;
-      }
+			V.DY = BUOYANCY + GRAVITY;
 
-      V.DY = BUOYANCY + GRAVITY;
+			X += V.DX * GameTime.delta();
+			Y += V.DY * GameTime.delta();
 
-      X += V.DX * GameTime.delta();
-      Y += V.DY * GameTime.delta();
+			CURRENT = RIGHT;
+			CURRENT.runAnimation();
 
-      CURRENT = RIGHT;
-      CURRENT.runAnimation();
+			// Simulate a slow leak in the balloon
+			BUOYANCY += 1;
+		}
+	}
 
-      // Simulate a slow leak in the balloon
-      BUOYANCY += 1;
-    }
-  }
+	public boolean collision() {
+		boolean col = false;
 
-  public boolean collision()
-  {
-    boolean col = false;
+		for (GameObject go : PlayState.handler.getObjects()) {
+			if (go != this && go.getID() != ObjectID.Background
+					&& (Y + (HEIGHT / 2) - 14) >= go.getY() && X > go.getX()
+					&& X < go.getX() + go.getWidth()) {
+				col = true;
 
-    for (GameObject go : PlayState.handler.getObjects())
-    {
-      if (go != this && go.getID() != ObjectID.Background && (Y + (HEIGHT / 2) - 14) >= go.getY()
-          && X > go.getX() && X < go.getX() + go.getWidth())
-      {
-        col = true;
+				Y = (go.getY() - (HEIGHT / 2) + 14);
+				V.DY = 0;
 
-        Y = (go.getY() - (HEIGHT / 2) + 14);
-        V.DY = 0;
+				if (V.DX > 0)
+					V.DX -= (V.DX * 0.01);
+			}
+		}
 
-        if (V.DX > 0)
-          V.DX -= (V.DX * 0.01);
-      }
-    }
+		return col;
+	}
 
-    return col;
-  }
+	public void render(Graphics g) {
+		if (ALIVE) {
+			Graphics2D g2d = (Graphics2D) g.create();
 
-  public void render(Graphics g)
-  {
-    if (ALIVE)
-    {
-      Graphics2D g2d = (Graphics2D) g;
+			// BufferedImage image = art.hueImg(CURRENT.getAnimationFrame(),
+			// WIDTH, HEIGHT, HUE);
+			debugOptions(g2d);
 
-      //BufferedImage image = art.hueImg(CURRENT.getAnimationFrame(), WIDTH, HEIGHT, HUE);
- 
-      g2d.drawImage(CURRENT.getAnimationFrame(), (int) (X - (WIDTH / 2)), (int) (Y - (HEIGHT / 2)), WIDTH + HEIGHT/3, HEIGHT, null);
-    }
-  }
-  
-  /**
-   * initialize Animations
-   */
-  private void initAnimations(){
-	  
-	    RIGHT = new Animation(10, art.getSpriteFrame(spriteID.HANK,0), art.getSpriteFrame(spriteID.HANK,1));
-	    CURRENT = new Animation(3, art.getSpriteFrame(spriteID.HANK,0));
-}
+			g2d.drawImage(CURRENT.getAnimationFrame(), (int) (X - (WIDTH / 2)),
+					(int) (Y - (HEIGHT / 2)), WIDTH + HEIGHT / 3, HEIGHT, null);
+		}
+	}
+
+	private void debugOptions(Graphics2D g2d) {
+		initDebug();
+		g2d.setColor(Color.BLACK);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		float relativeZeroX = (X - Game.WIDTH / 3);
+		float relativeZeroY = (Y - (Game.HEIGHT * 2 / 3));
+		int row = 1;
+		for (String x : debugOptions) {
+			g2d.drawString(x, relativeZeroX, relativeZeroY + 11 * row);
+			row++;
+		}
+
+	}
+	private void initDebug() {
+		debugOptions = new LinkedList<String>();
+		String BouyancyDebug = new String().concat("Bouyancy = "
+				+ (int) BUOYANCY);
+		String PositionDebug = new String().concat("XPosition: " + (int) X
+				+ " || YPosition: " + (int) Y);
+		debugOptions.add(BouyancyDebug);
+		debugOptions.add(PositionDebug);
+	}
+
+	/**
+	 * initialize Animations
+	 */
+	private void initAnimations() {
+
+		RIGHT = new Animation(10, art.getSpriteFrame(spriteID.HANK, 0),
+				art.getSpriteFrame(spriteID.HANK, 1));
+		CURRENT = new Animation(3, art.getSpriteFrame(spriteID.HANK, 0));
+	}
 }
