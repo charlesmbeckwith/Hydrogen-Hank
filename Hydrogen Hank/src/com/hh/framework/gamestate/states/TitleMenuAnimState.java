dbbcf8da59020d9d5b4e6f3704af875610a10516
franticdeli <mark.schlottke@gmail.com>
2014-02-27 09:31:15 -0600

    Fix to the Title Menu Animation.

--- Hydrogen Hank/src/com/hh/framework/gamestate/states/TitleMenuAnimState.java
+++ Hydrogen Hank/src/com/hh/framework/gamestate/states/TitleMenuAnimState.java
@@ -27,51 +27,34 @@ public class TitleMenuAnimState extends GameState
   {
 	  if(!scrollingTitlesRunning)
 		  handler.tick();
-
   }
 
   public void render(Graphics g)
   {
-	  
-	
     g.drawImage(art.mainBg, 0, 0, Game.WIDTH, Game.HEIGHT, null);
 
     //Draw Hank animation
     g.drawImage(art.getSpriteFrame(spriteID.HANK, 0), hankFlyingPosition+=2, (int) ( Game.HEIGHT - hankFlyingPosition), 150, 150, null);
     if(hankFlyingPosition > Game.WIDTH){
     	hankFlyingPosition = 0;
-    }
-    
-    	
+    }    	
     
     if(scrollingTitlesRunning){
-    g.drawImage(art.mainTitle,
-        (Game.WIDTH / 2 - art.mainTitle.getWidth() / 2), position + 20, null);
-    g.drawImage(art.newButton,
-        (Game.WIDTH / 2 - art.newButton.getWidth() / 2), position + Game.HEIGHT - 275,
-
-        null);
-
-    g.drawImage(art.scoresButton,
-        (Game.WIDTH / 2 - art.scoresButton.getWidth() / 2),
-        position + Game.HEIGHT - 200, null);
-    g.drawImage(art.creditsButton,
-        (Game.WIDTH / 2 - art.creditsButton.getWidth() / 2), position + Game.HEIGHT
-            - 125, null);
+	    g.drawImage(art.mainTitle,
+	        (Game.WIDTH / 2 - art.mainTitle.getWidth() / 2), position + 20, null);
+	    g.drawImage(art.newButton,
+	        (Game.WIDTH / 2 - art.newButton.getWidth() / 2), position + 300, null);
+	    g.drawImage(art.scoresButton,
+	        (Game.WIDTH / 2 - art.scoresButton.getWidth() / 2), position + 375, null);
+	    g.drawImage(art.creditsButton,
+	        (Game.WIDTH / 2 - art.creditsButton.getWidth() / 2), position + 450, null);
     }else{
-    	 g.drawImage(art.mainTitle,
-    		        (Game.WIDTH / 2 - art.mainTitle.getWidth() / 2), 20, null);
-
-    		    handler.render(g);
-    }
-
-
-    
+    	g.drawImage(art.mainTitle,(Game.WIDTH / 2 - art.mainTitle.getWidth() / 2), 20, null);
+    	handler.render(g);
+    }    
 
     if (position <= 0)
     {
-    	
-      //Game.manager.STATES.pop();
     	scrollingTitlesRunning = false;
     	
         handler.addObject(new MenuButton(art.newButton, art.newButton2,
@@ -83,6 +66,8 @@ public class TitleMenuAnimState extends GameState
         handler.addObject(new MenuButton(art.creditsButton, art.creditsButton2,
             Game.WIDTH / 2, Game.HEIGHT - 125, art.creditsButton.getWidth(),
             art.creditsButton.getHeight(),ButtonID.CREDITS));
+        
+        Game.manager.pop();
     }else{
     	position -= 3;
     }
