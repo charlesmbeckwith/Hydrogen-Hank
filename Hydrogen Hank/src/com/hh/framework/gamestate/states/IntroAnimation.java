563595c1ea34f7fb362035d6cd09ecae90420c2c
franticdeli <mark.schlottke@gmail.com>
2014-02-27 18:26:33 -0600

    Slowed down the IntroAnimation

--- Hydrogen Hank/src/com/hh/framework/gamestate/states/IntroAnimation.java
+++ Hydrogen Hank/src/com/hh/framework/gamestate/states/IntroAnimation.java
@@ -77,6 +77,14 @@ public class IntroAnimation extends GameState {
 			redBoxHeight+=4;
 		}
 
+		try
+    {
+      Thread.sleep(10);
+    } catch (InterruptedException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 	}
 
 	@Override
