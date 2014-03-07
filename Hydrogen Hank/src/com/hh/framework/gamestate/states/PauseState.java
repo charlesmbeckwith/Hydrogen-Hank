0eee61594762e852f45514f22ee726934c4ff900
franticdeli <mark.schlottke@gmail.com>
2014-03-07 17:04:34 -0600

    Pause screen was shaking again...whats causing that??

--- Hydrogen Hank/src/com/hh/framework/gamestate/states/PauseState.java
+++ Hydrogen Hank/src/com/hh/framework/gamestate/states/PauseState.java
@@ -15,6 +15,9 @@ public class PauseState extends GameState
 
   public void render(Graphics g)
   {
+	  // TODO: Find a better fix for the shaky pause screen...
+	Game.playState.render(g); // Solves the shaky pause screen issue
+	
     g.drawImage(art.pauseScreen,
         (Game.WIDTH / 2 - art.pauseScreen.getWidth() / 2), (Game.HEIGHT / 2 - 150), null);
   }
