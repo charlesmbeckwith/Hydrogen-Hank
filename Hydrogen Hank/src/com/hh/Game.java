26383bb55b8c7ba04ccf55daf13bd0d22651d31d
Solarnum <charlie.beckwith@marquette.edu>
2014-04-04 17:28:24 -0500

    Some goods

--- Hydrogen Hank/src/com/hh/Game.java
+++ Hydrogen Hank/src/com/hh/Game.java
@@ -31,7 +31,7 @@ public class Game extends Canvas implements Runnable
 {
 	private static final long serialVersionUID = 5679202415177178318L;
 	private static final int MIL = 1000000;
-	public static final int SLEEPTIME = 10;
+	public static final int SLEEPTIME = 20;
 	public static GameStateManager manager;
 	public static PlayState playState;
 	public static int width, height, heightOffset;
